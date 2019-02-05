ceaf16dabd01550847375b6131058127a0d0a053
joeappleton <joe.appleton@solent.ac.uk>
2019-02-05 15:48:40 +0000

    pulling

--- src/components/ShowData.vue
+++ src/components/ShowData.vue
@@ -1,26 +1,26 @@
 <template>
-  <div>
-    <button class="btn btn-primary" v-on:click="show = !show">Click me to show the H1</button>
+<div class="row">
     
-    <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="https://dummyimage.com/600x400/000/fff" alt="Card image cap">
+     <div class="col-12">  <h1 class='text-center'>Places to Visit</h1>   </div>
+    <div v-for="result in results" class="col-4">
+      <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="" alt="Card image cap">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
+    </div>
+    <!-- ./col-6 -->
+    
+  
+
+</div>
+<!-- ./row -->
     
     
-    <h1 v-show="show">Hello</h1>
-    <ul>
-         <li v-for="result in results"> 
-             {{result}}
-         </li>  
-         
-         
-      
-    </ul>
+    
    
   </div>
 </template>
@@ -40,8 +40,8 @@
       }
     },
     mounted: function() {
-    
-      const inst = axios.create({headers: { 'Authorization': 'Bearer '+ this.key} })
+
+      const inst = axios.create({ headers: { 'Authorization': 'Bearer ' + this.key } })
       inst.get('https://api.autoura.com/api/stops/search?group_context=friends&stop_types=food').then(r => {
         this.results = r.data.response;
       }).catch(e => {
